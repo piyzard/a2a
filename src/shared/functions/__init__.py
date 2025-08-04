ae8742ab75768bbf2d52d8161360e08aba86129d
piyzard <piyushgithub1223@gmail.com>
2025-08-04 22:57:37 +0530

    feat: add issue templates for bug reports and feature requests, and configure pull request template

--- src/shared/functions/__init__.py
+++ src/shared/functions/__init__.py
@@ -2,11 +2,9 @@
 
 from ..base_functions import function_registry
 from .kubeconfig import KubeconfigFunction
-
-# Multi-cluster functions will be imported when they exist
-# from .multicluster_create import MultiClusterCreateFunction
-# from .multicluster_logs import MultiClusterLogsFunction  
-# from .deploy_to import DeployToFunction
+from .multicluster_create import MultiClusterCreateFunction
+from .multicluster_logs import MultiClusterLogsFunction
+from .deploy_to import DeployToFunction
 
 
 def initialize_functions():
@@ -14,9 +12,9 @@ def initialize_functions():
     # Register kubeconfig function
     function_registry.register(KubeconfigFunction())
     
-    # Register KubeStellar multi-cluster functions (when implemented)
-    # function_registry.register(MultiClusterCreateFunction())
-    # function_registry.register(MultiClusterLogsFunction())
-    # function_registry.register(DeployToFunction())
+    # Register KubeStellar multi-cluster functions
+    function_registry.register(MultiClusterCreateFunction())
+    function_registry.register(MultiClusterLogsFunction())
+    function_registry.register(DeployToFunction())
     
     # Add more function registrations here as they are created
